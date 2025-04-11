bbd0549859a8a2d74bed0e8561b40140f18fa25c
mintr124 <129513108+mintr124@users.noreply.github.com>
2025-04-11 09:43:20 +0700

    Update PetResourceTest.java

--- spring-petclinic-customers-service/src/test/java/org/springframework/samples/petclinic/customers/web/PetResourceTest.java
+++ spring-petclinic-customers-service/src/test/java/org/springframework/samples/petclinic/customers/web/PetResourceTest.java
@@ -25,6 +25,8 @@ import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import org.springframework.test.util.ReflectionTestUtils;
+
 
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(PetResource.class)
